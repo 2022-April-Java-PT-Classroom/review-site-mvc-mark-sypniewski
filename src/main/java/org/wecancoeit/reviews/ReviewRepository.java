595099d00d5cef3a0806e30ff4fc885fb8687297
mark-sypniewski <msyp721@gmail.com>
2022-06-10 01:16:42 -0400

    Nope

---
+++ src/main/java/org/wecancoeit/reviews/ReviewRepository.java
@@ -0,0 +1,23 @@
+package org.wecancoeit.reviews;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class ReviewRepository {
+
+    Map<Long, Review> reviewsList = new HashMap<>();
+
+    public ReviewRepository(Review... reviewsToAdd) {
+        for (Review review : reviewsToAdd) {
+            reviewsList.put(review.getId(), review);
+        }
+    }
+
+    public Review findOne(long id) {return reviewsList.get(id); }
+
+    public Collection<Review> findAll() {return reviewsList.values();}
+}
